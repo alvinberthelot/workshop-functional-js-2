e121c225cfc4bb6562ab75dc88d4f8601e9d2d0e
Alvin Berthelot <alvin.berthelot@webyousoon.com>
2018-03-01 13:22:26 +0100

    Fix test for exercise 3 with 2 pairs

--- test/exercise3.test.js
+++ test/exercise3.test.js
@@ -1,30 +1,28 @@
-const expect = require('chai').expect;
-const injectStar = require('../src/exercise3').injectStar;
+const expect = require("chai").expect;
+const injectStar = require("../src/exercise3").injectStar;
 
-describe('GIVEN function injectStar', function() {
-
-  it('WHEN string is empty THEN should return an empty string', function() {
-    const input = '';
-    const output = '';
+describe("GIVEN function injectStar", function() {
+  it("WHEN string is empty THEN should return an empty string", function() {
+    const input = "";
+    const output = "";
     expect(injectStar(input)).to.eql(output);
   });
 
-  it('WHEN string is not empty and has one pair THEN should return appropriate string', function() {
-    const input = 'hello';
-    const output = 'hel*lo';
+  it("WHEN string is not empty and has one pair THEN should return appropriate string", function() {
+    const input = "hello";
+    const output = "hel*lo";
     expect(injectStar(input)).to.eql(output);
   });
 
-  it('WHEN string is not empty and has two pairs THEN should return appropriate string', function() {
-    const input = 'xxyy';
-    const output = 'xx*yy';
+  it("WHEN string is not empty and has two pairs THEN should return appropriate string", function() {
+    const input = "xxyy";
+    const output = "x*xy*y";
     expect(injectStar(input)).to.eql(output);
   });
 
-  it('WHEN string is not empty and has only pairs THEN should return appropriate string', function() {
-    const input = 'aaaa';
-    const output = 'a*a*a*a';
+  it("WHEN string is not empty and has only pairs THEN should return appropriate string", function() {
+    const input = "aaaa";
+    const output = "a*a*a*a";
     expect(injectStar(input)).to.eql(output);
   });
-
 });
